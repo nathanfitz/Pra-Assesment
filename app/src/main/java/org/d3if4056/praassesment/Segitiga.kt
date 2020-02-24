05305dc0a6fc9e57b45a0543010621d305d89c85
nathanfitz <nfitzgerald98@gmail.com>
2020-02-24 11:59:26 +0700

    Initial Commit

---
+++ app/src/main/java/org/d3if4056/praassesment/Segitiga.kt
@@ -0,0 +1,24 @@
+package org.d3if4056.praassesment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Segitiga : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_segitiga, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/org/d3if4056/praassesment/TitleFragment.kt
@@ -0,0 +1,44 @@
+package org.d3if4056.praassesment
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import org.d3if4056.praassesment.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_title, container, false)
+
+        binding.btnPersegi.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_persegiPanjang))
+        binding.btnSegitiga.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_segitiga))
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+        view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+
+
+}
